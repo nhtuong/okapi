cc67dc6c5209b209a13de4a7adfcb25b3eb8bc85
nhtuong <nhtuong@gmail.com>
2013-08-19 10:13:07 +0200

    Okapi Eclipse Project

---
+++ src/main/java/org/mc/okapi/Import.java
@@ -0,0 +1,199 @@
+package org.mc.okapi;
+
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableModel;
+
+public class Import extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField tffilename;
+	public JFileChooser chooser;
+	public String datafile;
+	private MainFrame mf;
+	public StringMatrix datmat;
+	public CSVTableView tw;
+	public DefaultTableModel data;
+	public String[][] dada;
+	public String delim;
+	/**
+	 * Create the frame.
+	 */
+	public Import(MainFrame mf) {
+		
+		this.mf = mf;
+		
+		setTitle("Importing...");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("src/images/ico/extra/science_32.png"));
+		setBounds(100, 100, 392, 138);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblDelimiter = new JLabel("Delimiter:");
+		lblDelimiter.setBounds(10, 11, 67, 14);
+		contentPane.add(lblDelimiter);
+		
+		JLabel lblFile = new JLabel("File:");
+		lblFile.setBounds(36, 36, 46, 14);
+		contentPane.add(lblFile);
+		chooser = new JFileChooser(System.getProperty("user.dir"));
+		JButton btnBrowse = new JButton("Browse...");
+		btnBrowse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+
+				
+				
+			  	chooser.addChoosableFileFilter(new FileNameExtensionFilter("*.txt","txt"));
+			  	chooser.addChoosableFileFilter(new FileNameExtensionFilter("*.csv","csv"));
+			  	
+			  	//System.out.println(chooser.getSelectedFile());
+			  	
+				if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+					
+					String workingdir = chooser.getCurrentDirectory().getPath();
+					System.out.println(workingdir);
+					
+					chooser.setCurrentDirectory(new File(workingdir));
+				  	
+				  	datafile = chooser.getSelectedFile().getAbsolutePath();
+				  	tffilename.setText(datafile);
+				}else {
+					
+				}
+				
+				
+			}
+		});
+		btnBrowse.setBounds(261, 32, 89, 23);
+		contentPane.add(btnBrowse);
+		
+		tffilename = new JTextField();
+		tffilename.setBounds(87, 33, 176, 20);
+		contentPane.add(tffilename);
+		tffilename.setColumns(10);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+
+		btnCancel.setBounds(199, 66, 89, 23);
+		contentPane.add(btnCancel);
+		
+		final JRadioButton rbSemicoma = new JRadioButton(";");
+		rbSemicoma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				delim = rbSemicoma.getText();
+			}
+		});
+		rbSemicoma.setBounds(83, 7, 34, 23);
+		contentPane.add(rbSemicoma);
+		
+		final JRadioButton rbComma = new JRadioButton(",");
+		rbComma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				delim = rbComma.getText();
+			}
+		});
+		rbComma.setBounds(118, 7, 29, 23);
+		contentPane.add(rbComma);
+		
+		final JRadioButton rbTab = new JRadioButton("\\t");
+		rbTab.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				delim = rbTab.getText();
+			}
+		});
+		rbTab.setBounds(149, 7, 40, 23);
+		contentPane.add(rbTab);
+		
+		//Group the radio buttons.
+	    ButtonGroup group = new ButtonGroup();
+	    group.add(rbSemicoma);
+	    group.add(rbComma);
+	    group.add(rbTab);
+	    
+
+
+	    
+		JButton btnImport = new JButton("Import...");
+		btnImport.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				  //create TableView
+				tw = new CSVTableView();
+	            
+	            FileReader fin = null;
+				try {
+					fin = new FileReader(datafile);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				
+	            data = tw.createTableModel(fin, null,delim);
+	            
+	            
+				//CSVReader reader = new CSVReader(new FileReader(datafile));
+				//datmat = reader.readAll();
+				
+				dada = new String[data.getRowCount()][data.getColumnCount()];
+				for (int i=0;i<data.getRowCount();i++){
+					for (int j=0;j<data.getColumnCount();j++){								
+						dada[i][j] = data.getValueAt(i,j).toString();								
+					}
+				}
+				
+				try {
+					Update();
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InstantiationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				dispose();
+			}
+		});
+		btnImport.setBounds(84, 64, 89, 23);
+		contentPane.add(btnImport);
+		
+
+		
+	
+	}
+	
+	private void Update() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
+		this.mf.UpdateTablePane(dada, data, tw);
+	}
+}
---
+++ src/main/java/org/mc/okapi/UndoManagerTableModel.java
@@ -0,0 +1,189 @@
+package org.mc.okapi;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JTable;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import javax.swing.undo.UndoManager;
+
+/**
+ * Basic implementation of a 
+ * @author Thierry LEFORT
+ * 11 févr. 08
+ *
+ */
+public class UndoManagerTableModel extends UndoManager implements TableModel {
+	/** Undo KeyStroke : Control + Z */
+	public static final KeyStroke UNDO_KEY = KeyStroke.getKeyStroke(
+			KeyEvent.VK_Z, 
+			InputEvent.CTRL_DOWN_MASK);
+ 	/** Redo KeyStroke : Control + Y */
+	public static final KeyStroke REDO_KEY = KeyStroke.getKeyStroke(
+			KeyEvent.VK_Y, 
+			InputEvent.CTRL_DOWN_MASK); 
+	/**
+	 * @see UIManager#getString(Object)
+	 * @return the UIManager String for <code>"AbstractUndoableEdit.undoText"</code>
+	 */
+	public static final String getUndoText() {
+		return UIManager.getString("AbstractUndoableEdit.undoText");
+	}
+	/**
+	 * @see UIManager#getString(Object)
+	 * @return the UIManager String for <code>"AbstractUndoableEdit.redoText"</code>
+	 */
+	public static final String getRedoText() {
+		return UIManager.getString("AbstractUndoableEdit.redoText");
+	}
+	
+	/**
+	 * Install an UndoManager on a JTable
+	 * @see #install(JTable, TableModel)
+	 * @param table the table
+	 * @return the Installed Manager
+	 */
+	public static UndoManager install(JTable table) {
+		return install(table, table.getModel());
+	}
+	/**
+	 * Install an UndoManager on the given table with the given model.
+	 * @param table the table
+	 * @param model the model
+	 * @return the installed manager
+	 */
+	public static UndoManager install(JTable table, TableModel model) {
+		final UndoManagerTableModel manager = new UndoManagerTableModel(model);
+		/* UNDO */
+		String undo = getUndoText();
+		Action undoAction = new AbstractAction(undo) {
+			public void actionPerformed(ActionEvent e) {
+				if (manager.canUndo()) {
+					manager.undo();
+				}
+			}
+		};
+		table.getActionMap().put(undo, undoAction);
+		table.getInputMap().put(UNDO_KEY, undo);
+		/* REDO */
+		String redo = getRedoText();
+		Action redoAction = new AbstractAction(redo) {
+			public void actionPerformed(ActionEvent e) {
+				if (manager.canRedo()) {
+					manager.redo();
+				}
+			}
+		};
+		table.getActionMap().put(redo, redoAction);
+		table.getInputMap().put(REDO_KEY, redo);
+		
+		table.setModel(manager);
+		return manager;
+	}
+	/**
+	 * Remove all Undo settings from the table 
+	 * @param table the table
+	 */
+	public static void uninstall(JTable table) {
+		table.getActionMap().remove(getUndoText());
+		table.getActionMap().remove(getRedoText());
+		table.getInputMap().remove(UNDO_KEY);
+		table.getInputMap().remove(REDO_KEY);
+		TableModel model = table.getModel();
+		if (model instanceof UndoManagerTableModel) {
+			UndoManagerTableModel undoModel = (UndoManagerTableModel) model;
+			table.setModel(undoModel._modelDelegate);
+		}
+	}
+	 
+	
+	/**
+	 * Model delegator
+	 */
+	private TableModel _modelDelegate;
+	/**
+	 * Default constructor 
+	 * @param model delegator
+	 */
+	public UndoManagerTableModel(TableModel model) {
+		_modelDelegate = model;
+	}
+	/**
+	 * {@link TableModel#addTableModelListener(TableModelListener)}
+	 */
+	public void addTableModelListener(TableModelListener l) {
+		_modelDelegate.addTableModelListener(l);
+	}
+	/**
+	 * {@link TableModel#getColumnClass(int)}
+	 */
+	public Class<?> getColumnClass(int columnIndex) {
+		return _modelDelegate.getColumnClass(columnIndex);
+	}
+	/**
+	 * {@link TableModel#getColumnCount()}
+	 */
+	public int getColumnCount() {
+		return _modelDelegate.getColumnCount();
+	}
+	/**
+	 * {@link TableModel#getColumnName(int columnIndex)}
+	 */
+	public String getColumnName(int columnIndex) {
+		return _modelDelegate.getColumnName(columnIndex);
+	}
+	/**
+	 * {@link TableModel#getRowCount()}
+	 */
+	public int getRowCount() {
+		return _modelDelegate.getRowCount();
+	}
+	/**
+	 * {@link TableModel#getValueAt(int rowIndex, int columnIndex)}
+	 */
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		return _modelDelegate.getValueAt(rowIndex, columnIndex);
+	}
+	/**
+	 * {@link TableModel#isCellEditable(int rowIndex, int columnIndex)}
+	 */
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return _modelDelegate.isCellEditable(rowIndex, columnIndex);
+	}
+	/**
+	 * {@link TableModel#removeTableModelListener(TableModelListener l)}
+	 */
+	public void removeTableModelListener(TableModelListener l) {
+		_modelDelegate.removeTableModelListener(l);
+	}
+	/**
+	 * {@link TableModel#setValueAt(Object, int, int)}
+	 * 
+	 * Store the old and new value into a {link CellUndoableEdit} and add it,
+	 * {@link UndoManager#addEdit(javax.swing.undo.UndoableEdit).
+	 */
+	public void setValueAt(Object value, int rowIndex, int columnIndex) {
+		Object oldValue = getValueAt(rowIndex, columnIndex);
+		_modelDelegate.setValueAt(value, rowIndex, columnIndex);
+		//It is important to make the setValueAt before adding the edit in the UndoManager,
+		//if you fail to set the value you don't want to add it. 
+		CellUndoableEdit edit = 
+			new CellUndoableEdit(
+					_modelDelegate, 
+					rowIndex, 
+					columnIndex, 
+					oldValue, 
+					value);
+		addEdit(edit);
+		
+	}
+	
+}
+
+
